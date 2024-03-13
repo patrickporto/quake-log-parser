2ec66d4d97d7e8e9b802b958b51efeaa1ef71322
Patrick Porto <patrick.s.porto@gmail.com>
2024-03-12 22:46:52 -0600

    feat: add recognize arguments

--- quake_log_parser/transforms/tokenization.py
+++ quake_log_parser/transforms/tokenization.py
@@ -4,9 +4,13 @@ from enum import Enum
 
 class Entity(str, Enum):
     UNKNOWN = 'unknown'
+    WORD = 'word'
+    DELIMITER = 'delimiter'
+    NUMBER = 'number'
     TIME = 'time'
     COMMAND = 'command'
     COMMAND_ARGUMENT = 'command_argument'
+    KEYWORD = 'keyword'
 
 
 @dataclass
@@ -30,55 +34,104 @@ COMMAND_TOKENS = [
     'Item'
 ]
 
+KEYWORDS = [
+    'killed',
+    'by'
+]
+
+DELIMITERS = [
+    ':',
+    ' ',
+    '\n',
+]
+
+
 
 class Tokenizer:
     def tokenize(self, log_record):
         tokens = []
-        log_record_token_index = 0
-        token_entity = Entity.UNKNOWN
-        while log_record_token_index < len(log_record):
-            record_token = log_record[log_record_token_index]
-            if record_token == '\n':
-                log_record_token_index += 1
-                continue
-            previous_token = tokens[-1].value if len(tokens) > 0 else ''
-            if record_token == ':' and previous_token.isdigit():
-                tokens[-1].append_value(record_token)
-                tokens[-1].entity = Entity.TIME
-                log_record_token_index += 1
-                continue
-            if record_token == ' ':
-                tokens.append(Token(
-                    entity=token_entity
-                ))
-                log_record_token_index += 1
-                continue
-            if record_token == ':':
-                token_entity = Entity.COMMAND_ARGUMENT
-                tokens.append(Token(
-                    entity=Entity.COMMAND_ARGUMENT,
-                ))
-                log_record_token_index += 1
-                continue
-            if len(tokens) == 0:
-                tokens.append(Token(
-                    value=record_token,
-                    entity=token_entity,
-                ))
-                log_record_token_index += 1
-                continue
-            tokens[-1].append_value(record_token)
-            log_record_token_index += 1
+        for character in list(log_record):
+            token = self.get_token(character)
+            tokens.append(token)
+        tokens = self.recognize_words(tokens)
+        tokens = self.recognize_numbers(tokens)
+        tokens = self.recognize_time(tokens)
+        tokens = self.recognize_commands(tokens)
+        tokens = self.recognize_keywords(tokens)
+        tokens = self.recognize_arguments(tokens)
+        return self.clean_tokens(tokens)
 
-        cleaned_tokens = self.clean_tokens(tokens)
-        return self.recognize_entities(cleaned_tokens)
+    def get_token(self, character):
+        if character.isdigit():
+            return Token(entity=Entity.NUMBER, value=character)
+        elif character.isalpha() or character in ['_', '<', '>']:
+            return Token(entity=Entity.WORD, value=character)
+        elif character in DELIMITERS:
+            return Token(entity=Entity.DELIMITER, value=character)
+        return Token(entity=Entity.UNKNOWN, value=character)
 
-    def recognize_entities(self, tokens):
+    def clean_tokens(self, tokens):
+        return [token for token in tokens if token.value.strip() != '']
+
+    def recognize_words(self, tokens):
+        result = []
+        last_token = None
+        for token in tokens:
+            if token.entity == Entity.WORD and last_token and last_token.entity == Entity.WORD:
+                result[-1].append_value(token.value)
+            else:
+                result.append(token)
+            last_token = token
+        return result
+
+    def recognize_numbers(self, tokens):
+        result = []
+        last_token = None
+        for token in tokens:
+            if token.entity == Entity.NUMBER and last_token and last_token.entity == Entity.NUMBER:
+                result[-1].append_value(token.value)
+            else:
+                result.append(token)
+            last_token = token
+        return result
+
+    def recognize_time(self, tokens):
+        result = []
+        last_token = None
+        is_time = False
+        for token in tokens:
+            if token.entity == Entity.NUMBER and last_token and last_token.value == ':':
+                is_time = True
+                result.pop()
+                result[-1].append_value(':')
+                result[-1].entity = Entity.TIME
+            if token.entity == Entity.NUMBER and is_time:
+                result[-1].append_value(token.value)
+            else:
+                is_time = False
+                result.append(token)
+            last_token = token
+        return result
+
+    def recognize_commands(self, tokens):
         for token in tokens:
             if token.value in COMMAND_TOKENS:
                 token.entity = Entity.COMMAND
         return tokens
 
+    def recognize_keywords(self, tokens):
+        for token in tokens:
+            if token.value in KEYWORDS:
+                token.entity = Entity.KEYWORD
+        return tokens
 
-    def clean_tokens(self, tokens):
-        return [token for token in tokens if token.value != '']
+    def recognize_arguments(self, tokens):
+        result = []
+        is_argument = False
+        for token in tokens:
+            if token.entity == Entity.COMMAND:
+                is_argument = True
+            if is_argument and token.entity not in [Entity.COMMAND, Entity.DELIMITER, Entity.KEYWORD]:
+                token.entity = Entity.COMMAND_ARGUMENT
+            result.append(token)
+        return result
--- tests/transforms/test_tokenization.py
+++ tests/transforms/test_tokenization.py
@@ -11,14 +11,14 @@ def test_empty_log_record():
 
 def test_simple_text():
     log_record = 'Hello'
-    expected_tokens = [Token(value='Hello')]
+    expected_tokens = [Token(value='Hello', entity=Entity.WORD)]
     tokenizer = Tokenizer()
     assert tokenizer.tokenize(log_record) == expected_tokens
 
 
 def test_simple_number():
     log_record = '1234'
-    expected_tokens = [Token(value='1234')]
+    expected_tokens = [Token(value='1234', entity=Entity.NUMBER)]
     tokenizer = Tokenizer()
     assert tokenizer.tokenize(log_record) == expected_tokens
 
@@ -32,7 +32,11 @@ def test_time():
 
 def test_time_and_command():
     log_record = '  0:00 InitGame:'
-    expected_tokens = [Token(value='0:00', entity=Entity.TIME), Token(value='InitGame', entity=Entity.COMMAND)]
+    expected_tokens = [
+        Token(value='0:00', entity=Entity.TIME),
+        Token(value='InitGame', entity=Entity.COMMAND),
+        Token(value=':', entity=Entity.DELIMITER),
+    ]
     tokenizer = Tokenizer()
     assert tokenizer.tokenize(log_record) == expected_tokens
 
@@ -42,8 +46,28 @@ def test_command_and_arguments():
     expected_tokens = [
         Token(value='20:40', entity=Entity.TIME),
         Token(value='Item', entity=Entity.COMMAND),
+        Token(value=':', entity=Entity.DELIMITER),
         Token(value='2', entity=Entity.COMMAND_ARGUMENT),
         Token(value='weapon_rocketlauncher', entity=Entity.COMMAND_ARGUMENT),
+    ]
+    tokenizer = Tokenizer()
+    assert tokenizer.tokenize(log_record) == expected_tokens
+
+def test_kill_command():
+    log_record = '21:42 Kill: 1022 2 22: <world> killed Isgalamido by MOD_TRIGGER_HURT'
+    expected_tokens = [
+        Token(value='21:42', entity=Entity.TIME),
+        Token(value='Kill', entity=Entity.COMMAND),
+        Token(value=':', entity=Entity.DELIMITER),
+        Token(value='1022', entity=Entity.COMMAND_ARGUMENT),
+        Token(value='2', entity=Entity.COMMAND_ARGUMENT),
+        Token(value='22', entity=Entity.COMMAND_ARGUMENT),
+        Token(value=':', entity=Entity.DELIMITER),
+        Token(value='<world>', entity=Entity.COMMAND_ARGUMENT),
+        Token(value='killed', entity=Entity.KEYWORD),
+        Token(value='Isgalamido', entity=Entity.COMMAND_ARGUMENT),
+        Token(value='by', entity=Entity.KEYWORD),
+        Token(value='MOD_TRIGGER_HURT', entity=Entity.COMMAND_ARGUMENT),
     ]
     tokenizer = Tokenizer()
     assert tokenizer.tokenize(log_record) == expected_tokens
