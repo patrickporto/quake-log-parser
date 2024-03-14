4607a5d106b50f6099cf17845b9914ba813a8800
Patrick Porto <patrick.s.porto@gmail.com>
2024-03-14 01:15:07 -0600

    fix: import database in main

--- quake_log_parser/__main__.py
+++ quake_log_parser/__main__.py
@@ -2,6 +2,7 @@ from quake_log_parser.cli import cli
 from quake_log_parser import source  # noqa
 from quake_log_parser import player_ranking  # noqa
 from quake_log_parser import ingestion  # noqa
+from quake_log_parser import database  # noqa
 
 
 def main():
